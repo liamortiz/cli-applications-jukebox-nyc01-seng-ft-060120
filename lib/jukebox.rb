15b7bd17af44a521fd5802fbe2462eba28051e5c
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-14 01:47:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,7 +18,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
   
-  if not songs.include?(user_input) and not (user_input.to_i).between?(1, songs.size)
+  if not songs.include?(user_input) or not (user_input.to_i).between?(1, songs.size)
     puts "Invalid input, please try again"
   end
   if songs.include?(user_input)
