7e59c3e6747ae0bdfbb25b80637166eca621f333
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-14 01:27:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,11 @@
 # Add your code here
 
 def help
-  puts ""
+  puts "I accept the following commands: 
+  \n- help : displays this help message
+  \n- list : displays a list of songs you can play
+  \n- play : lets you choose a song to play
+  \n- exit : exits this program"
 end
 
 def list
